5ed047cc0b12e28831c0c338c27b04febd83eac8
CalebStephen18 <123010683+CalebStephen18@users.noreply.github.com>
2023-09-16 22:59:49 +0530

    Update train.py

--- train.py
+++ train.py
@@ -17,7 +17,7 @@ y = iris.target
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Train a Random Forest classifier
-clf = Perceptron(max_iter = 1)
+clf = Perceptron(max_iter = 5)
 clf.fit(X_train, y_train)
 
 # Save the trained model
